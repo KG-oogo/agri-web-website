6681da84c954cb8a687186a3f0be133ac2c616a1
molepoka <molepoka@webmail.co.za>
2020-06-20 18:33:09 +0200

    Updates

---
+++ precache-manifest.15414a98eecf7f74d5eed3384cd7a18b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df241aaa823ebe81a42c1c467e57767a",
+    "url": "/agri-web-website/index.html"
+  },
+  {
+    "revision": "2e93226a395d52195773",
+    "url": "/agri-web-website/static/css/main.85b8a94e.chunk.css"
+  },
+  {
+    "revision": "3d1baf9f9bcc0a6a3879",
+    "url": "/agri-web-website/static/js/2.997f40da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/agri-web-website/static/js/2.997f40da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e93226a395d52195773",
+    "url": "/agri-web-website/static/js/main.fe491a13.chunk.js"
+  },
+  {
+    "revision": "6635b647b7d4a68b28dd",
+    "url": "/agri-web-website/static/js/runtime-main.b6d97907.js"
+  },
+  {
+    "revision": "31a61ad5268311880ac6d1a798b8e6b3",
+    "url": "/agri-web-website/static/media/logo.31a61ad5.jpg"
+  }
+]);
